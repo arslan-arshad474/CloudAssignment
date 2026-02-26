e6858054e528b57ff8ea4a3543257efeb73ea2ee
Mirza Arslan <20093854@mydbs.ie>
2026-02-26 23:14:55 +0000

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# This block automatically finds the latest Ubuntu 24.04 AMI in us-east-1
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's official AWS ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "docker_sg" {
+  name        = "docker_server_sg_new" # Changed name to avoid conflicts
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "docker_server" {
+  ami           = data.aws_ami.ubuntu.id # Uses the ID found above
+  instance_type = "t3.micro"
+  key_name      = "Assignment_Key"
+
+  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y docker.io
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ubuntu
+              EOF
+
+  tags = {
+    Name = "DockerServer"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.docker_server.public_ip
+}
